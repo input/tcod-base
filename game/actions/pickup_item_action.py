4240c0909ff2fc594d4113d776402993a71e6061
Laurence Mercer <laurencemercer@googlemail.com>
2024-11-28 18:07:06 +0000

    Fix check map when picking up items.

--- game/actions/pickup_item_action.py
+++ game/actions/pickup_item_action.py
@@ -5,7 +5,7 @@ import tcod.ecs
 from game.actions.base_action import BaseAction
 from game.components import Position
 from game.managers import inventory_manager
-from game.tags import IsItem
+from game.tags import IsIn, IsItem
 
 
 class PickupItemAction(BaseAction):
@@ -13,5 +13,6 @@ class PickupItemAction(BaseAction):
 
     def __call__(self, actor: tcod.ecs.Entity) -> None:
         # Pickup any items the actor has stepped on.
-        for item in actor.registry.Q.all_of(tags=[actor.components[Position], IsItem]):
+        map = actor.relation_tag[IsIn]
+        for item in actor.registry.Q.all_of(tags=[actor.components[Position], IsItem], relations=[(IsIn, map)]):
             inventory_manager.add_item(actor, item)
